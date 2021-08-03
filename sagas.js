fcca8fdc9ce917c1c9f43bc3a826a5650afb5ba0
Nauzet Hernandez <Nauzet@ADEdelivered.com>
2021-08-03 11:39:40 +0100

    ADD: test and FIX: sagas Tutorial bullshit

--- sagas.js
+++ sagas.js
@@ -1,12 +1,12 @@
-import { put, takeEvery, all } from "redux-saga/effects";
+import { put, takeEvery, all, call } from "redux-saga/effects";
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export function* helloSaga() {
   console.log("Hello buorl");
 }
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
   yield put({ type: "INCREMENT" });
 }
 
